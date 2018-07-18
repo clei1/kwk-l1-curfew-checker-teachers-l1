420f0cffb8d0c31ec6ac5d35c49732b30f3583c8
clei1 <callmecon328@gmail.com>
2018-07-18 22:46:39 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -29,6 +29,15 @@ end
 
 def deluxe_curfew_checker(time)
   # code goes here
+  if(time > 10)
+    if(time == 11)
+      puts "You're in trouble! Better get home quick!"
+    else
+      puts "Time to apparate!"
+    end
+  else
+    puts "Keep having fun!"
+  end
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
